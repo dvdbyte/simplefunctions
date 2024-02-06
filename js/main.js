79f12467888f9c2b4d7abc7f331dc1337f1dad68
dvdbyte <dvdbyte1@gmail.com>
2024-02-06 17:54:07 +0000

    version-1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//create a function that adds two numbers and alerts the sum
+function addTwoNumbers(a, b){
+  return a + b
+}
+
+//create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(a, b, c){
+  return a * b * c
+}
+//create a function that divides two numbers and returns the ???
+function dividTwoNumbers(a, b){
+  return a/b
+}
+console.log(addTwoNumbers(5, 8))
+console.log(multiplyThreeNumbers(2, 4, 5))
+console.log(dividTwoNumbers(345, 5))
